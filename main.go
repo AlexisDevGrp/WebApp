f0d0cfa1226cb652f21fb9c3c7ccc834681236c6
Alexis Brochier <alexisdevgrp@gmail.com>
2019-01-03 20:37:10 +0000

    Added contact page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my dirt site</h1>")
+	fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my dirt site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:frankyparki@gmail.com\">Support</a>.")
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
