79ebed40a6285bfa34cc05fd5b41f5f82526936e
Alexis Brochier <alexisdevgrp@gmail.com>
2019-01-19 14:39:27 +0000

    bootstrap

--- views/view.go
+++ views/view.go
@@ -3,18 +3,23 @@ package views
 import "html/template"
 
 // NewView manages all used views
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 // View struc definition
 type View struct {
 	Template *template.Template
+	Layout   string
 }
